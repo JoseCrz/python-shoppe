d4d4907e60c50794a409a65771290929c1f6c7e7
José Cuevas <josecueram@gmail.com>
2019-02-04 14:05:30 -0600

    Crated update client functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 clients = "José,Sara,"
 
-# Function that lists all the clients
+
 def list_clients ():
+    """Lists all the clients"""
+
     global clients
     print(clients)
 
@@ -17,7 +19,23 @@ def create_client (client_name):
         list_clients()
     else:
         print("Client already exists!")
+
+
+def update_client (client_name):
+    """Searches the clients list for the name given, if it exists, asks the user for the updated version"""
+    
+
+    global clients 
+    if client_name not in clients:
+        print("Client not found!")
         
+    else:
+        updated_client_name = input("What's the new name?: ")
+        clients = clients.replace(client_name + ",", updated_client_name + ",")
+        print("Update successful!")
+        list_clients()
+
+
 
 def delete_client ():
     pass
@@ -34,7 +52,7 @@ def _print_welcome ():
     """Private function that prints a menu"""
 
     print("Welcome to the Shoppe Administration")
-    print("*" * 50)
+    print("-" * 50)
     print("What would you like to do?")
     print("[C]reate client")
     print("[U]pdate")
@@ -49,12 +67,19 @@ def _get_client_name () :
 if __name__ == "__main__":
     _print_welcome()
     command = input("type letter: ")
+    command = command.upper()
 
     if command == "C":
+        print("***** Create Client *****")
+
         client_name = _get_client_name()
+        create_client(client_name)
     
     elif command == "U":
+        print("***** Update Client *****")
+
         client_name = _get_client_name()
+        update_client(client_name)
 
     elif command == "D":
         delete_client()
