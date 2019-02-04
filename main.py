aa24fb572923bb16bbfa3e7ddbc79ac5efc8cae9
José Cuevas <josecueram@gmail.com>
2019-02-04 09:58:09 -0600

    Added basic functions to add & list clients

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-clients = "José, Sara"
+clients = "José,Sara,"
+
+# Function that lists all the clients
+def list_clients ():
+    global clients
+    print(clients)
+
+
+#Function that creates a client 
+def create_client (client_name):
+    global clients
+    clients += client_name
+    _add_comma()
+
+
+# Private functions that adds a comma to separate each client on the list
+def _add_comma ():
+    global clients
+    clients += ","
+
 
 if __name__ == "__main__":
-    clients += ", Eduardo"
-    print(clients)
+    list_clients()
+    create_client("Mich")
+    list_clients()
+
